6c4ff1d6e017b637323f25ecd93941ae4b36fab5
siyuan.zhu <siyuan.zhu@zhaopin.com.cn>
2019-09-26 19:40:00 +0800

    999

---
+++ main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.append(r'./ui/')
+import ui
+
+ui.initWindow()
---
+++ package/config.py
@@ -0,0 +1,9 @@
+
+# -*- coding: utf-8 -*-
+
+def getConfig():
+    return {
+        "appkey": 'TXH74wgv56oGoFFF',
+        "token": '97c5f8ab3f2a4b7c91804ec9605d5c3e'
+    }
+
